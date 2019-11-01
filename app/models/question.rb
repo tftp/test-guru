127edac42eb0ef08c7994e606143ed9cc267ea11
Oleg Goloborodko <goloborodko@inbox.ru>
2019-11-01 15:51:40 +0300

    add associations for models

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,4 @@
 class Question < ApplicationRecord
+  belongs_to :tests
+  has_many :answers
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,5 @@
 class Test < ApplicationRecord
+  belongs_to :categories
+  has_many :questions
+  has_and_belongs_to_many :users
 end
