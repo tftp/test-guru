63afbdf7aefb59831c3a9c42479b1e8312d3312b
Oleg Goloborodko <goloborodko@inbox.ru>
2019-11-01 18:07:43 +0300

    add new method for Test class

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,4 +2,14 @@ class Test < ApplicationRecord
 #  belongs_to :category
 #  has_many :questions
   has_and_belongs_to_many :users
+
+  def self.list_tests_for_category(name)
+    list = []
+    id = Category.find_by(title: name).id
+    Test.where(categories_id: id).order('title DESC').each do |test|
+      list << test.title
+    end
+    return list
+  end
+  
 end
