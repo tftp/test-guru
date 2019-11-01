eadec43d09e7ffeac3440edd0a0cce20340eb041
Oleg Goloborodko <goloborodko@inbox.ru>
2019-11-01 17:41:48 +0300

    add new method for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :tests
+
+  def list_test(level)
+    Test.where(id: self.tests.ids).each do |test|
+      puts test.title if test.level == level.to_i
+    end
+  end
+  
 end
