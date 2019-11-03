511862785dc8b64b913c51b61f85e977611395a2
Oleg Goloborodko <goloborodko@inbox.ru>
2019-11-03 22:52:59 +0300

    corrected method for Test model

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,7 +4,7 @@ class Test < ApplicationRecord
   has_and_belongs_to_many :users
 
   def self.list_tests_for_category(name)
-    Test.joins('JOIN categories ON tests.category_id == categories.id').where(categories.title: name).order('title DESC').pluck(:title)
+    Category.joins(:tests).where(title: name).order('tests.title DESC').pluck('tests.title')
   end
 
 end
