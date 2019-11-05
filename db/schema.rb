22d9835bb64223aa575a18659ac0540d23d79f54
Oleg Goloborodko <goloborodko@inbox.ru>
2019-11-05 20:54:13 +0300

    add associate tests author users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_142210) do
+ActiveRecord::Schema.define(version: 2019_11_05_163715) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2019_11_05_142210) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "level", default: 1
     t.integer "category_id"
+    t.integer "author"
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
@@ -60,6 +61,8 @@ ActiveRecord::Schema.define(version: 2019_11_05_142210) do
 
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
+  add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users", column: "author"
   add_foreign_key "tests_users", "tests"
   add_foreign_key "tests_users", "users"
 end
