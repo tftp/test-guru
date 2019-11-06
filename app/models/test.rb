6e0634e725b68390c3eb88d3a8a3f0f302074ec0
Oleg Goloborodko <goloborodko@inbox.ru>
2019-11-06 17:34:04 +0300

    corrected models

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
   belongs_to :category
-  belongs_to :author, class_name: "User", foreign_key: "id"
+  belongs_to :author, class_name: "User" , foreign_key: "id"
   has_many :questions
   has_and_belongs_to_many :users
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :tests
-  has_many :tests_create, class_name: "Test", foreign_key: "author"
+  has_many :my_tests, class_name: "Test", foreign_key: "author"
 
   def list_test(level)
     self.tests.where(level: level)
