60fae5293b975674b2bfa9dede3758d7254a7283
Oleg Goloborodko <goloborodko@inbox.ru>
2019-11-10 15:14:49 +0300

    correct methods

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,8 +1,15 @@
 class Answer < ApplicationRecord
-  belongs_to :question, inverse_of: :answers
+  belongs_to :question
 
   scope :true_answers, -> {where(correct: true)}
 
   validates :body, presence: true
+  validate :include_answers
+
+  private
+
+  def include_answers
+    errors.add(:question) unless question.answers.count.in?(1..4)
+  end
 
 end
