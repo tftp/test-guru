7d790a8f20d24eecd5458067595562209851ce0b
Oleg Goloborodko <goloborodko@inbox.ru>
2019-11-10 18:17:00 +0300

    add unique index to tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_08_152555) do
+ActiveRecord::Schema.define(version: 2019_11_10_151304) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2019_11_08_152555) do
     t.integer "category_id"
     t.integer "author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
   end
 
   create_table "tests_users", id: false, force: :cascade do |t|
