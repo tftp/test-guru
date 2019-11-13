89db9c3de382993b69ce278a9c5959f7e2d69931
Oleg Goloborodko <goloborodko@inbox.ru>
2019-11-13 18:11:42 +0300

    correct method create

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -21,13 +21,20 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    render plain: 'saved!'
+    @test = Test.find(params[:test_id])
+    @question = @test.questions.new(parameters_questions)
+    @question.save
+    redirect_to test_questions_path
   end
 
   private
 
   def rescue_with_question_not_found
     render plain: 'Question was not found'
+  end
+
+  def parameters_questions
+    params.require(:question).permit(:body, :test_id)
   end
 
 end
