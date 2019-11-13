62e6b983ab36e2867191f56718c1ca00cb6dc3d6
Oleg Goloborodko <goloborodko@inbox.ru>
2019-11-13 19:14:50 +0300

    add before action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,19 +1,19 @@
 class QuestionsController < ApplicationController
 
+  before_action :test_find, only: %i[index create]
+  before_action :question_find, only: %i[show destroy]
+
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
 #    result = ["Class: #{params.class}", "Parameters:#{params.inspect} "]
 #    render plain: result.join("\n")
-    @test = Test.find(params[:test_id])
     render plain: @test.questions.to_ary
-
   end
 
   def show
     @question = Question.find(params[:id])
     render html: @question.body
-
   end
 
   def new
@@ -21,18 +21,26 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    @test = Test.find(params[:test_id])
+#    @test = Test.find(params[:test_id])
     @question = @test.questions.new(parameters_questions)
     @question.save
     redirect_to test_questions_path
   end
 
   def destroy
-    @question = Question.find(params[:id])
+#    @question = Question.find(params[:id])
     @question.delete
   end
 
   private
+
+  def test_find
+    @test = Test.find(params[:test_id])
+  end
+
+  def question_find
+    @question = Question.find(params[:id])
+  end
 
   def rescue_with_question_not_found
     render plain: 'Question was not found'
