4f871b94c6f27469a91fb9464c0ff4e7dbe1d262
Oleg Goloborodko <goloborodko@inbox.ru>
2019-11-15 11:59:37 +0300

    add new views and correct controllers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,25 +6,21 @@ class QuestionsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
-#    result = ["Class: #{params.class}", "Parameters:#{params.inspect} "]
-#    render plain: result.join("\n")
-    render plain: @test.questions.to_ary
+    @questions = @test.questions.all
   end
 
   def show
-#    @question = Question.find(params[:id])
-    render html: @question.body
+
   end
 
   def new
-
+    @question = Question.new
   end
 
   def create
-#    @test = Test.find(params[:test_id])
     @question = @test.questions.new(parameters_questions)
     if @question.save
-      redirect_to test_questions_path
+      redirect_to @question
     else
       render :new
     end
