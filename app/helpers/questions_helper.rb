3c6ae7959828bf377a4af76478b036dd4a2d0212
Oleg Goloborodko <goloborodko@inbox.ru>
2019-11-15 19:50:18 +0300

    add helpers and footer

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,16 @@
 module QuestionsHelper
+
+  def question_header(id)
+    test = Test.find(id)
+    test.title
+  end
+
+  def current_year
+    Time.current.year
+  end
+
+  def github_url(author, repo)
+    url = ['https://github.com'] << author << repo
+    url.join('/')
+  end
 end
