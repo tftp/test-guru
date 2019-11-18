ba7d532a9f8967c9ef5002c5ffeea15ec7917bb2
Oleg Goloborodko <goloborodko@inbox.ru>
2019-11-18 21:00:59 +0300

    correct models controllers migrations

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,16 @@ Rails.application.routes.draw do
     resources :questions, shallow: true do
         resources :answers, shallow: true, except: :index
     end
-
+    member do
+      post :start
+    end
   end
+
+
+  resources :test_passages, only: %i[show update] do
+    member do
+      get :result
+    end
+  end
+
 end
