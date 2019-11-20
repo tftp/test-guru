b1a804a008915794664f8c8289e40ca386d8c0f9
Oleg Goloborodko <goloborodko@inbox.ru>
2019-11-20 21:02:34 +0300

    correct test_passage accept!

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -11,7 +11,7 @@ class TestPassagesController < ApplicationController
   end
 
   def update
-    @test_passage.accept(params[:answer_ids])
+    @test_passage.accept!(params[:answer_ids])
 
     if @test_passage.completed?
       redirect_to result_test_passage_path(@test_passage)
