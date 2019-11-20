9722349c00ac1800e96accd0df3204d720b66eca
Oleg Goloborodko <goloborodko@inbox.ru>
2019-11-20 20:20:04 +0300

    add user model controller authentificate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
+require 'digest/sha1'
+
 class User < ApplicationRecord
+
+  include Auth
+
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :my_tests, class_name: "Test", foreign_key: "author_id"
-
-  validates :email, presence: true
 
   def list_test(level)
     self.tests.where(level: level)
@@ -12,6 +15,5 @@ class User < ApplicationRecord
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
   end
-
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_133640) do
+ActiveRecord::Schema.define(version: 2019_11_20_163031) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -59,10 +59,11 @@ ActiveRecord::Schema.define(version: 2019_11_19_133640) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email", default: "", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "answers", "questions"
