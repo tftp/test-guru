595478af99e1d92f8de5c45c49be86a602e0d682
Oleg Goloborodko <goloborodko@inbox.ru>
2019-11-25 20:17:13 +0300

    add Single Table Inheritance admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_24_151220) do
+ActiveRecord::Schema.define(version: 2019_11_25_164254) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -76,9 +76,11 @@ ActiveRecord::Schema.define(version: 2019_11_24_151220) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "type", default: "User", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["type"], name: "index_users_on_type"
   end
 
   add_foreign_key "answers", "questions"
