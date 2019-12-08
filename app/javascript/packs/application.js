249edf365f9e47ce6b4f8f683323b86fd69ec2c6
Oleg Goloborodko <goloborodko@inbox.ru>
2019-12-08 16:16:47 +0300

    add validate for password confirmation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,12 @@ require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
-import sort from '../helpers/sorting';
+
+require("helpers/sorting");
+require("helpers/validinput");
+
+//import  '../helpers/sorting';
+//import  '../helpers/validating';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
