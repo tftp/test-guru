723f1d1c38c74878369b9d088480b17e58709c5f
Oleg Goloborodko <goloborodko@inbox.ru>
2019-12-11 19:02:26 +0300

    add indexes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_04_174439) do
+ActiveRecord::Schema.define(version: 2019_12_11_152149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2019_12_04_174439) do
     t.integer "level", default: 1
     t.integer "category_id"
     t.integer "author_id"
+    t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
     t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
   end
