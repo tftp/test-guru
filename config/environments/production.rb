a73e6754229934bb57c700b129c187d844152de6
Oleg Goloborodko <goloborodko@inbox.ru>
2019-12-12 20:58:22 +0300

    add smtp config to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,8 +63,8 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'fast-cliffs-91491.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_setting = {
-    adress:                'smtp.gmail.com',
+  config.action_mailer.smtp_settings = {
+    address:                'smtp.gmail.com',
     port:                   587,
     user_name:              ENV['SMTP_USERNAME'],
     password:               ENV['SMTP_PASSWORD'],
