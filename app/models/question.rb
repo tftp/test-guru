5becdea0b6a7f7d4286e92da4d3f607412922114
Oleg Goloborodko <goloborodko@inbox.ru>
2019-12-15 17:00:18 +0300

    corrected dependences

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,7 @@ class Question < ApplicationRecord
   belongs_to :test
   has_many :answers, inverse_of: :question, dependent: :destroy
   has_many :gists, dependent: :destroy
+  has_many :test_passages, foreign_key: "current_question", dependent: :destroy
 
   validates :body, presence: true
 end
