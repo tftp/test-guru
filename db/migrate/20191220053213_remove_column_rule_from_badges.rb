96f896294dfd5f556fe38dedba08653e9c4058cc
Oleg Goloborodko <goloborodko@inbox.ru>
2019-12-23 20:26:39 +0300

    corrected migrations

--- db/migrate/20191220053213_remove_column_rule_from_badges.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnRuleFromBadges < ActiveRecord::Migration[6.0]
-  def change
-    remove_column(:badges, :rule)
-  end
-end
--- db/migrate/20191220170738_drop_badges_users.rb
+++
@@ -1,5 +0,0 @@
-class DropBadgesUsers < ActiveRecord::Migration[6.0]
-  def up
-    drop_table :badges_users, if_exists: true
-  end
-end
