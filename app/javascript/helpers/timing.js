0099a5baadcb31e1b60252c4a83b3ff0267f9f49
Oleg Goloborodko <goloborodko@inbox.ru>
2020-01-02 10:05:12 +0300

    draft timer js

--- app/javascript/helpers/timing.js
+++ app/javascript/helpers/timing.js
@@ -1,11 +1,25 @@
 document.addEventListener('turbolinks:load', function() {
   var control = document.querySelector('.timer')
-  if (control.innerText !== 0) { timerPassing() }
+  if (control.innerText !== 0) { startTimer() }
 })
 
-function timerPassing(){
+function startTimer(){
   var timer = document.querySelector('.timer');
   var time = timer.innerText;
-  console.log(time);
-
+  var arr = time.split(":")
+  var m = arr[0];
+  var s = arr[1];
+  if (s == 0) {
+    if (m == 0) {
+      alert ("Time out");
+      return;
+    }
+    m--;
+    if (m < 10) m = "0" + m;
+    s = 59;
+  }
+  else s--;
+  if (s < 10) s = "0" + s;
+  document.querySelector('.timer').innerText = m+":"+s;
+  setTimeout(startTimer, 1000);
 }
